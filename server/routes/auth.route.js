30866b09961fb1de2d74b643891389d8130b0fcf
Tanmay Yadav <tanmayyadav2323@gmail.com>
2023-02-17 12:27:20 +0530

    Feat: Authentication Server

---
+++ server/routes/auth.route.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+
+
+const checkAuth = require("../middlewares/checkAuth");
+const checkAdmin = require("../middlewares/checkAdmin");
+
+const {
+  fetchCurrentUser,
+  loginWithPhoneOtp,
+  createNewUser,
+  verifyPhoneOtp,
+  handleAdmin
+} = require("../controllers/auth.controller");
+
+
+router.post("/register", createNewUser);
+
+router.post("/login_with_phone", loginWithPhoneOtp);
+
+
+router.post("/verify", verifyPhoneOtp);
+
+router.get("/me", checkAuth, fetchCurrentUser);
+
+router.get("/admin", checkAuth, checkAdmin, handleAdmin);
+
+module.exports = router;
